2c05c8827861daae08d0585145f3777a44509d78
shenwei235 <shenwei235@gmail.com>
2013-10-08 11:36:46 +0800

    load or require basing on Rails.configuration.cache_class

--- lib/spree_last_address.rb
+++ lib/spree_last_address.rb
@@ -7,7 +7,7 @@ module SpreeLastAddress
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env.production? ? require(c) : load(c)
+        Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
 
